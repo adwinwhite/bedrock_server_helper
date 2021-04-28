de034e14f38395164f3a2d1aea544b862b089228
AdwinKing <jin2000112014@gmail.com>
2021-04-28 15:30:25 +0800

    fixed download url for papyrucs

--- rendermap.sh
+++ rendermap.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-PRED="$(pwd)"
 DIR="$(dirname "${BASH_SOURCE[0]}")"  # Get the directory name
 DIR="$(realpath "${DIR}")"
-PAPY_DOWNLOAD_URL="https://github.com/mjungnickel18/papyruscs/releases/download/v0.5.1/papyruscs-dotnetcore-0.5.1-linux64.zip"
+
 
 PAPY_DIR="$(realpath "${DIR}/../papyruscs")"
-if [ ! -d "PAPY_DIR" ]; then
-  cd "${DIR}/.."
-  wget "$PAPY_DOWNLOAD_URL" -o "papyruscs.zip"
+WEB_DIR="$PAPY_DIR/www/map"
+if [ ! -d "$PAPY_DIR" ]; then
+  pushd "${DIR}/.."
+  curl -s https://api.github.com/repos/mjungnickel18/papyruscs/releases/latest \
+  | grep "browser_download_url" \
+  | cut -d '"' -f 4 \
+  | wget -qi - -o "papyruscs.zip"
+  # wget "$PAPY_DOWNLOAD_URL" -o "papyruscs.zip"
   if [ ! -d "$PAPY_DIR" ]; then
     mkdir "$PAPY_DIR"
   fi
   unzip "papyruscs.zip" -d "$PAPY_DIR"
   chmod +x "$PAPY_DIR/PapyrusCs"
-  cd "$PRED"
+  popd
 fi
 
 # render the map
+if [ ! -d "$WEB_DIR" ]; then
+  mkdir -r "$WEB_DIR"
+fi
 echo "正在更新网页地图..."
-$PAPY_DIR/PapyrusCs --world "$DIR/worlds/Bedrock level/db" --dim 0 --output "/var/www/map" --htmlfile "index.html"
-$PAPY_DIR/PapyrusCs --world "$DIR/worlds/Bedrock level/db" --dim 1 --output "/var/www/map" --htmlfile "index.html"
-$PAPY_DIR/PapyrusCs --world "$DIR/worlds/Bedrock level/db" --dim 2 --output "/var/www/map" --htmlfile "index.html"
+$PAPY_DIR/PapyrusCs --world "$DIR/worlds/Bedrock level/db" --dim 0 --output "$WEB_DIR" --htmlfile "index.html"
+$PAPY_DIR/PapyrusCs --world "$DIR/worlds/Bedrock level/db" --dim 1 --output "$WEB_DIR" --htmlfile "index.html"
+$PAPY_DIR/PapyrusCs --world "$DIR/worlds/Bedrock level/db" --dim 2 --output "$WEB_DIR" --htmlfile "index.html"
 echo "网页地图更新完毕"
